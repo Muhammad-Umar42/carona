6292e0a7859140ce11d4f2a174cc7ae002b8d69f
mumar <umrking8877@gmail.com>
2022-09-15 18:23:30 +0500

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
+import Country from './component/country'
 import Covid from './component/Covid'
+import Graph from './component/graph'
 
 function App() {
+  const [confrm, setConfrm] = useState([])
+  const [dead, setDead] = useState([])
+  const [recover, setRecover] = useState([])
+  const [isCountry, setIsCountry] = useState([])
+  const [globalConfrm, setGlobalConfrm] = useState([])
+  const [globalDead, setGlobalDead] = useState([])
+  const [globalRecover, setGlobalRecover] = useState([])
+
+  // console.log(globalDead)
 
   return (
     <>
-      <Covid />
+      <Covid confrm={confrm} recover={recover} dead={dead} isCountry={isCountry}
+        setGlobalConfrm={setGlobalConfrm} setGlobalDead={setGlobalDead} setGlobalRecover={setGlobalRecover} />
+      <Country setConfrm={setConfrm} setRecover={setRecover} setDead={setDead} setIsCountry={setIsCountry} />
+      <Graph confrm={confrm} recover={recover} dead={dead}
+        globalConfrm={globalConfrm} globalDead={globalDead} globalRecover={globalRecover} />
     </>
   )
 }
